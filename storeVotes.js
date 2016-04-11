16386d42de6615652f3e401ea3e16c94e632f91d
Rikki Hansen <rikkihansen@Rikkis-MacBook-Air.local>
2016-04-10 17:19:31 -0700

    a lot of work

--- storeVotes.js
+++ storeVotes.js
@@ -1,43 +1,43 @@
 // staring script
-var Product = function (img, name) {
+var Product = function (img, name, idx) {
   this.img= img;
   this.voteCount=0;
-  this.name= name;
+  this.label= name;
+  this.idx=idx;
+  this.y=0;
 };
-
-
-var image1= new Product("bag.jpg", "bag");
-var image2= new Product("banana.jpg", "banana");
-var image3= new Product("boots.jpg", "boots");
-var image4= new Product("chair.jpg", "chair");
-var image5= new Product("cthulhu.jpg", "cthulhu");
-var image6= new Product("dragon.jpg", "dragon");
-var image7= new Product("pen.jpg", "pen");
-var image8= new Product("scissors.jpg", "scissors");
-var image9= new Product("shark.jpg", "skark");
-var image10= new Product("sweep.jpg", "sweep");
-var image11= new Product("unicorn.jpg", "unicorn");
-var image12= new Product("usb.jpg", "usb");
-var image13= new Product("water_can.jpg", "water_can");
-var image14= new Product("wine_glass.jpg", "wine_glass");
 
 //making imageArray
 var imageArray= new Array ();
-imageArray.push(new Product("bag.jpg", "bag"));
-imageArray.push(new Product("banana.jpg", "banana"));
-imageArray.push(new Product("boots.jpg", "boots"));
-imageArray.push(new Product("chair.jpg", "chair"));
-imageArray.push(new Product("cthulhu.jpg", "cthulhu"));
-imageArray.push(new Product("dragon.jpg", "dragon"));
-imageArray.push(new Product("pen.jpg", "pen"));
-imageArray.push(new Product("scissors.jpg", "scissors"));
-imageArray.push(new Product("shark.jpg", "shark"));
-imageArray.push(new Product("sweep.jpg", "sweep"));
-imageArray.push(new Product("unicorn.jpg", "unicorn"));
-imageArray.push(new Product("usb.jpg", "usb"));
-imageArray.push(new Product("water_can.jpg", "water_can"));
-imageArray.push(new Product("wine_glass.jpg", "wine_glass"));
+imageArray.push(new Product("bag.jpg", "bag", 0));
+imageArray.push(new Product("banana.jpg", "banana", 1));
+imageArray.push(new Product("boots.jpg", "boots", 2));
+imageArray.push(new Product("chair.jpg", "chair", 3));
+imageArray.push(new Product("cthulhu.jpg", "cthulhu", 4));
+imageArray.push(new Product("dragon.jpg", "dragon", 5));
+imageArray.push(new Product("pen.jpg", "pen", 6));
+imageArray.push(new Product("scissors.jpg", "scissors", 7));
+imageArray.push(new Product("shark.jpg", "shark", 8));
+imageArray.push(new Product("sweep.jpg", "sweep", 9));
+imageArray.push(new Product("unicorn.jpg", "unicorn", 10));
+imageArray.push(new Product("usb.jpg", "usb", 11));
+imageArray.push(new Product("water_can.jpg", "water-can", 12));
+imageArray.push(new Product("wine_glass.jpg", "wine-glass", 13));
 
+var availablePhotos = new Array();
+function makeArrayCopy() {
+  for(var index=0; index < imageArray.length; index++) {
+     availablePhotos.push(imageArray[index])
+  }
+}
+function randomPhoto() {
+  if (availablePhotos.length == 0) {
+    makeArrayCopy()
+ }
+  var index = Math.floor(Math.random() * availablePhotos.length);
+  var photoToUse = availablePhotos.splice(index,1)[0];
+     return photoToUse
+}
 
 
 function displayImage () {
@@ -45,72 +45,67 @@ function displayImage () {
   imageHolder.innerHTML = " ";
   for (var x=0; x < 3; x++) {
     var img = new Image();
-    var random = getRandomNum();
-    var randomImage = imageArray[random];
+    var randomImage = randomPhoto();
     img.src = randomImage.img;
-    img.dataset.idx = random;
+    img.dataset.idx = randomImage.idx;
     imageHolder.appendChild(img);
+    console.log(event);
   }
 };
 
-function getRandomNum() {
-  return Math.floor(Math.random() * imageArray.length)
-}
 var totalVotes = 0;
-var imageHolder = document.getElementById("imageHolder");
- imageHolder.addEventListener("click", function(event) {
+ imageHolder.addEventListener("click", function() {
+     imageHolder.innerHTML= " ";
      var clickedImage = imageArray[event.target.dataset.idx];
      var showButton= document.getElementById("button");
-     var totals = document.getElementById("total");
-     clickedImage.voteCount++;
+     var container = document.getElementById("chartContainer");
+     clickedImage.y++;
      totalVotes++;
      console.log(clickedImage);
      console.log(totalVotes);
      displayImage();
+     chart.render();
 
   if (totalVotes % 15 == 0) {
     showButton.style.display= "block";
   }
   else {
     showButton.style.display="none";
-    totals.style.display = "none";
-  }
- })
-
- var button= document.getElementById("button");
-button.addEventListener("click", function(event) {
-  var totals = document.getElementById('total');
-  var tbody = document.getElementById('total').children[1];
-  tbody.innerHTML = '';
-  totals.style.display = "table";
-  for (var x=0; x < imageArray.length; x++) {
-    var row = totals.insertRow(x + 1);
-    var cell1 = row.insertCell(0);
-    var cell2 = row.insertCell(1);
-    cell1.innerHTML = imageArray[x].name;
-    cell2.innerHTML = imageArray[x].voteCount;
+    container.style.display="none";
   }
 });
 
 
+var button= document.getElementById("button");
+ button.addEventListener("click", function() {
+var showButton= document.getElementById("button");
+var container = document.getElementById("chartContainer");
+if (showButton.onclick) {
+  container.style.display="block";
+}
+else {
+  container.style.display="none";
+  }
+});
 
 
+ var chart= null;
+window.onload = function () {
+	 chart = new CanvasJS.Chart("chartContainer", {
+		title:{
+			text: "Click's Per Photo"
+		},
+		data: [
+		{
+			type: "column",
+			dataPoints: imageArray
+
+		}
+		]
+	});
+	chart.render();
+}
 
 
-
-
-
-
-
-
- // while (randomImage < 3) {
- //   randomImage
- // }
-
-
-
-
-
-
-
-displayImage(image1);
+makeArrayCopy();
+displayImage();
